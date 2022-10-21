27695082f3a4307b0818b336f522ba8f3929787b
Connor Norton <connorn314@gmail.com>
2022-10-21 16:49:31 -0700

    render index for pokemon

--- pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
+++ pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
@@ -1,2 +1,37 @@
 class Api::PokemonController < ApplicationController
+
+    TYPES = [
+        'fire',
+        'electric',
+        'normal',
+        'ghost',
+        'psychic',
+        'water',
+        'bug',
+        'dragon',
+        'grass',
+        'fighting',
+        'ice',
+        'flying',
+        'poison',
+        'ground',
+        'rock',
+        'steel'
+    ].sort.freeze
+
+    def types 
+        render json: TYPES.to_json
+    end
+
+    def index
+        @pokemons = Pokemon.all
+        render :index
+    end
+
+    def show 
+        @pokemon = Pokemon.find_by(id: params[:id])
+        render :show
+    end
+    
+    
 end
